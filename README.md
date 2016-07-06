278698eb2dcbd0563d1a6459b968d36920bddc3a
johnpilbeam <john_pilbeam@yahoo.co.uk>
2016-07-06 12:49:52 +0100

    Add remaining theme files

--- README.md
+++
@@ -1,2 +0,0 @@
-# tibexus
-Drupal 8 theme for http://www.tibetanlaw.org website.
---
+++ theme-settings.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Implementation of hook_form_system_theme_settings_alter()
+ *
+ * @param $form
+ *   Nested array of form elements that comprise the form.
+ *
+ * @param $form_state
+ *   A keyed array containing the current state of the form.
+ */
+
+function tibexus_form_system_theme_settings_alter(&$form, &$form_state) {
+  $form['tibexus_settings'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('tibexus Settings'),
+    '#collapsible' => FALSE,
+    '#collapsed' => FALSE,
+  );
+  $form['tibexus_settings']['show_breadcrumbs'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Show breadcrumbs in a page'),
+    '#default_value' => theme_get_setting('show_breadcrumbs','tibexus'),
+    '#description'   => t("Check this option to show breadcrumbs in page. Uncheck to hide."),
+  );
+  $form['tibexus_settings']['slideshow'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Front Page Slideshow'),
+    '#collapsible' => TRUE,
+    '#collapsed' => FALSE,
+  );
+  $form['tibexus_settings']['slideshow']['slideshow_display'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Show slideshow'),
+    '#default_value' => theme_get_setting('slideshow_display','tibexus'),
+    '#description'   => t("Check this option to show Slideshow in front page. Uncheck to hide."),
+  );
+  $form['tibexus_settings']['slideshow']['slide'] = array(
+    '#markup' => t('You can change the description and URL of each slide in the following Slide Setting fieldsets.'),
+  );
+  $form['tibexus_settings']['slideshow']['slide1'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Slide 1'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+  );
+  $form['tibexus_settings']['slideshow']['slide1']['slide1_head'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Slide Headline'),
+    '#default_value' => theme_get_setting('slide1_head','tibexus'),
+  );
+  $form['tibexus_settings']['slideshow']['slide1']['slide1_desc'] = array(
+    '#type' => 'textarea',
+    '#title' => t('Slide Description'),
+    '#default_value' => theme_get_setting('slide1_desc','tibexus'),
+  );
+  $form['tibexus_settings']['slideshow']['slide1']['slide1_url'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Slide URL'),
+    '#default_value' => theme_get_setting('slide1_url','tibexus'),
+  );
+  $form['tibexus_settings']['slideshow']['slide2'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Slide 2'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+  );
+  $form['tibexus_settings']['slideshow']['slide2']['slide2_head'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Slide Headline'),
+    '#default_value' => theme_get_setting('slide2_head','tibexus'),
+  );
+  $form['tibexus_settings']['slideshow']['slide2']['slide2_desc'] = array(
+    '#type' => 'textarea',
+    '#title' => t('Slide Description'),
+    '#default_value' => theme_get_setting('slide2_desc','tibexus'),
+  );
+  $form['tibexus_settings']['slideshow']['slide2']['slide2_url'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Slide URL'),
+    '#default_value' => theme_get_setting('slide2_url','tibexus'),
+  );
+  $form['tibexus_settings']['slideshow']['slide3'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Slide 3'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+  );
+  $form['tibexus_settings']['slideshow']['slide3']['slide3_head'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Slide Headline'),
+    '#default_value' => theme_get_setting('slide3_head','tibexus'),
+  );
+  $form['tibexus_settings']['slideshow']['slide3']['slide3_desc'] = array(
+    '#type' => 'textarea',
+    '#title' => t('Slide Description'),
+    '#default_value' => theme_get_setting('slide3_desc','tibexus'),
+  );
+  $form['tibexus_settings']['slideshow']['slide3']['slide3_url'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Slide URL'),
+    '#default_value' => theme_get_setting('slide3_url','tibexus'),
+  );
+  $form['tibexus_settings']['slideshow']['slideimage'] = array(
+    '#markup' => t('To change the Slide Images, Replace the slide-image-1.jpg, slide-image-2.jpg and slide-image-3.jpg in the images folder of the theme folder.'),
+  );
+}
